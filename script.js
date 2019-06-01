f332bf8caf48511366453379e8a8530702284410
eduvelas <pedrovlst111@gmail.com>
2019-06-01 16:35:50 -0600

    es un ejemplo

---
+++ script.js
@@ -0,0 +1,15 @@
+var Pokemon = angular.module("Pokemon",[])
+Pokemon.controller("ListadoPokemon",function($scope,$rootScope,$http){
+	$scope.Lista = [];
+	for (var y = 1; y <= 100; y++) {
+		$http({
+		method: "GET",
+		url: "https://pokeapi.co/api/v2/pokemon/"+y
+	}).then(function datos(x){
+		$scope.Lista.push(x);
+	})
+}
+	})
+		
+
+	
